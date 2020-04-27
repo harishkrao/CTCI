ca9e44cbaac4c065868209a17b261160222a014a
Harish Kesava Rao <harish.kesavarao@gmail.com>
2020-04-27 07:21:28 -0500

    sample program template

---
+++ sampleprogram.py
@@ -0,0 +1,18 @@
+# imports here
+
+
+class sampleclass:
+    def some_method(self):
+        pass
+
+    def another_method(self):
+        pass
+
+    def run(self):
+        self.some_method()
+        self.another_method()
+
+
+if __name__ == "__main__":
+    ts = sampleclass()
+    ts.run()
