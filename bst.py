ce1bf45fe49eeac18d8c61ed795626628533da9b
Harish Kesava Rao <harish.kesavarao@gmail.com>
2020-05-10 15:27:20 -0500

    bst fixes

--- bst.py
+++ bst.py
@@ -31,13 +31,13 @@ class BST:
 
     def insertNode(self, val, rootval):
         while rootval.leftChild is not None and rootval.rightChild is not None:
-            if rootval > val:
+            if rootval.val > val:
                 rootval = rootval.rightChild
                 return self.insertNode(val, rootval)
-            elif rootval < val:
+            elif rootval.val < val:
                 rootval = rootval.leftChild
                 return self.insertNode(val, rootval)
-        if val > rootval:
+        if val > rootval.val:
             rootval.rightChild = Node(val)
         else:
             rootval.leftChild = Node(val)
@@ -51,10 +51,20 @@ class BST:
             return "found"
         else:
             while self.root.leftChild is not None and self.root.rightChild is not None:
-                if self.root > val:
+                if self.root.val > val:
                     rootval = rootval.leftChild
                     return self.findNode(val, rootval)
                 else:
                     rootval = rootval.rightChild
                     return self.findNode(val, rootval)
         return "not found"
+
+b = BST()
+b.setRoot(5)
+b.insert(4)
+b.insert(6)
+b.insert(2)
+b.insert(3)
+print(b.find(5))
+
+
