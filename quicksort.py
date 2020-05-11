26f0db287ee5891c2dc36b191de6593a85f3c170
Harish Kesava Rao <harish.kesavarao@gmail.com>
2020-05-11 08:55:47 -0500

    quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,38 +1,33 @@
 class quicksort:
-    def __init__(self, array):
-        self.array = array
-
-    def qsort(self, array, lo, hi):
+    def quicksort(self, array, lo, hi):
         if lo >= hi:
             return
         p = self.partition(array, lo, hi)
-        self.qsort(array, lo, p-1)
-        self.qsort(array, p+1, hi)
+        self.quicksort(array, lo, p-1)
+        self.quicksort(array, p+1, hi)
 
     def partition(self, array, lo, hi):
-        pivot = array[lo]
+        v = array[lo]
         start = lo + 1
         end = hi
 
         while True:
-            while start <= end and array[start] <= pivot:
+            while start <= end and array[start] <= v:
                 start += 1
-            print(start, end, array)
-            while start <= end and array[end] >= pivot:
+            while start <= end and array[end] >= v:
                 end -= 1
-            if start <= end:
-                array[start], array[end] = array[end], array[start]
-            else:
+            if start > end:
                 break
-        print(start, end)
-        array[end], array[lo] = array[lo], array[end]
+            else:    
+                array[start], array[end] = array[end], array[start]
+        array[lo], array[end] = array[end], array[lo]
         return end
 
 
 def main():
     array = [29, 99, 27, 41, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12, 21, 44]
-    q = quicksort(array)
-    q.qsort(array, 0, len(array)-1)
+    q = quicksort()
+    q.quicksort(array, 0, len(array)-1)
     print(array)
 
 
